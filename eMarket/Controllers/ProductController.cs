0c7cc84512401f1852569472dbc59133195dc5bd
orifjon9 <orifjon9@gmail.com>
2017-08-20 23:55:36 -0500

    the first sample for MVC + API

---
+++ eMarket/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using eMarket.DTO.Module;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eMarket.Controllers
+{
+    public class ProductController : Controller
+    {
+        private HttpClient client = new HttpClient();
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<ProductDTO> products = new List<ProductDTO>();
+
+            var getDataTask = client.GetAsync("http://localhost:52223/api/products")
+                .ContinueWith(response =>
+                {
+                    var result = response.Result;
+                    if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        var readResult = result.Content.ReadAsAsync<List<ProductDTO>>();
+                        readResult.Wait();
+
+                        products = readResult.Result;
+                    }
+                });
+            getDataTask.Wait();
+
+            return View(products);
+        }
+    }
+}
